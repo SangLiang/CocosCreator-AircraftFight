74b8d15ea33fed372794b730393a22b44ab4c21a
桑量的笔记本 <378305868@qq.com>
2019-07-11 22:52:53 +0800

    子弹的爆炸效果

--- assets/Script/Basic/Bullet.ts
+++ assets/Script/Basic/Bullet.ts
@@ -2,9 +2,13 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Bullet extends cc.Component {
+    @property(cc.Prefab)
+    exploreNode = null
+
     private attack: number = 1;
     private hp: number = 1;
     private speed: number = 400;
+
 
     public setAttack(attack: number) {
         this.attack = attack;
@@ -17,8 +21,16 @@ export default class Bullet extends cc.Component {
     underAttack() {
         this.hp -= 1;
         if (this.hp <= 0) {
+            this.explore();
             this.node.destroy();
+            
         }
+    }
+
+    explore(){
+        let _node =  cc.instantiate(this.exploreNode);
+        _node.setParent(cc.find('Canvas'));
+        _node.setPosition(this.node.position);
     }
 
     move(dt) {
